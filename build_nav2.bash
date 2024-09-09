49ce8cf97363a4235101fd2dc9ab7ed5f5c15f7b
weiqi-luo <weiqiluo0213@gmail.com>
2024-09-09 08:09:23 +0000

    add build nav2

---
+++ build_nav2.bash
@@ -0,0 +1,16 @@
+cd ../..
+if [ $# -eq 0 ]; then
+    ./scripts/ci/build_with_tests.sh --packages-select \
+    nav2_common \
+    nav2_msgs \
+    nav2_util \
+    nav2_lifecycle_manager \
+    nav2_map_server \
+    nav2_rviz_plugins \
+    nav2_amcl \
+    navigation2 \
+    nav2_bringup
+else
+    ./scripts/ci/build_with_tests.sh --packages-select $@
+fi
+cd -
