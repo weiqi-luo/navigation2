80509e93ead5cd15d985a36126a7818004462c1e
weiqi-luo <weiqiluo0213@gmail.com>
2025-01-15 08:21:23 +0000

    add launch for iw

---
+++ nav2_bringup/launch/iw_play_bag.py
@@ -0,0 +1,94 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Darby Lim
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("nav2_bringup")
+    map_yaml_file = LaunchConfiguration("map")
+    bag_file = LaunchConfiguration("bag")
+    qos_file = LaunchConfiguration("qos", default="/iw_data/config/qos/amcl.yaml")
+
+    use_rviz = LaunchConfiguration("use_rviz", default="true")
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+
+    param_dir = LaunchConfiguration(
+        "params_file",
+        default=os.path.join(
+            bringup_dir,
+            "params",
+            "nav2_params_iw.yaml",
+        ),
+    )
+
+    nav2_launch_file_dir = os.path.join(bringup_dir, "launch")
+
+    rviz_config_dir = os.path.join("/iw_data/config/rviz/iw_amcl.rviz")
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "map",
+                description="Full path to map file to load",
+            ),
+            DeclareLaunchArgument(
+                "params_file",
+                default_value=param_dir,
+                description="Full path to param file to load",
+            ),
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="false",
+                description="Use simulation (Gazebo) clock if true",
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                arguments=["-d", rviz_config_dir],
+                parameters=[{"use_sim_time": use_sim_time}],
+                condition=IfCondition(use_rviz),
+                output="screen",
+            ),
+            ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "bag",
+                    "play",
+                    bag_file,
+                    "-l",
+                    "--clock",
+                    "--qos-profile-overrides-path",
+                    qos_file,
+                    # "--remap",
+                    # "/tf:=/bag/tf",
+                    # "/tf_static:=/bag/tf_static",
+                ],
+                output="screen",
+            ),
+        ]
+    )
